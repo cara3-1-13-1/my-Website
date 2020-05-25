007a55e9b17fc9ac00673cf11fb3c044244cb6c6
cara3-1-13-1 <65281201+cara3-1-13-1@users.noreply.github.com>
2020-05-25 15:44:47 +0200

    added project subpage

---
+++ Material/2Roadtrip/Roadtrip.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE php>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name=“viewport” content=“width=device-width, initial-scale=1, user-scalable=yes”>
+        <title>Cara Kollmann</title>
+        <script src="packery.pkgd.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
+<body>
+     <footer>
+            <p><b>Nice, that you made it to the end!</b></p>
+            <p><b>Now let's have a coffee.</b></p>
+        </footer>
+  </body>
+</html>
