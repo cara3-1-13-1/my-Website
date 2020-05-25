e543c9d8f5c146ab298c4941c5be206458931c74
cara3-1-13-1 <65281201+cara3-1-13-1@users.noreply.github.com>
2020-05-25 15:45:41 +0200

    added external footer and header

--- header.php
+++ header.php
@@ -9,14 +9,16 @@
         <link rel="stylesheet" type="text/css" href="mystyle.css">
     </head>
 <body>
-     <div class="w3-content w3-opacity" style="max-width:1500px">
-        <header>
-            <div class="w3-bar">
-              <a href="index.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white"><h1><b>CARA KOLLMANN</b></h1></a>
-              <a href="Kontakt.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white w3-right"><h1><b>KONTAKT</b></h1></a>
-              <a href="About.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white w3-right"><h1><b>ABOUT</b></h1></a>
-            </div>
-        </header> 
-     </div>
+    <div class="content">
+         <div class="w3-content w3-top" style="max-width:1500px">
+            <header>
+                <div class="w3-bar">
+                  <a href="index.php" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white"><h1><b>CARA KOLLMANN</b></h1></a>
+                  <a href="Kontakt.php" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white w3-right"><h1><b>KONTAKT</b></h1></a>
+                  <a href="About.php" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white w3-right"><h1><b>ABOUT</b></h1></a>
+                </div>
+            </header> 
+         </div>
+    </div>
   </body>
 </html>
