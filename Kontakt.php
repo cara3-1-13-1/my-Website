52a5eb5c111b40d9dffa8fd51af992937f1a24d8
cara3-1-13-1 <65281201+cara3-1-13-1@users.noreply.github.com>
2020-05-25 11:53:44 +0200

    added subpages

---
+++ Kontakt.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name=“viewport” content=“width=device-width, initial-scale=1, user-scalable=yes”>
+        <title>Cara Kollmann</title>
+        <script src="packery.pkgd.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
+    
+   
+    
+<body>
+     <div class="w3-content w3-opacity" style="max-width:1500px">
+        <header>
+            <div class="w3-bar">
+              <a href="index.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white"><h1><b>CARA KOLLMANN</b></h1></a>
+              <a href="Kontakt.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white"><h1><b>KONTAKT</b></h1></a>
+              <a href="About.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white"><h1><b>ABOUT</b></h1></a>
+            </div>
+        </header> 
+     </div>
+        <!-- Packery -->
+        <div class="grid" data-packery='{ "itemSelector": ".grid-item", "gutter": 10 }'>    
+            <div class="grid">
+
+                <div class="grid-item grid-item--width2">
+                      <img src="Material/2Roadtrip/IMG_5251.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item grid-item--height2">
+                    <img src="Material/0Milla/Plakat%20A0%202-remastered2020.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item">
+                    <img src="Material/1Sternenkarte/Sternenkarte-7133.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item grid-item--width2 grid-item--height2">
+                    <img src="Material/3Type-Inspiration/IMG_5073.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item grid-item--width2 grid-item--height2">
+                    <img src="Material/5Coffea/Coffea-7241.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item grid-item--width2">
+                    <img src="Material/4This-is-Europe/ThisIsEurope-7163.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item grid-item--width2 grid-item--height2">
+                    <img src="Material/2PaperJam/20180720_KD-143.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item grid-item--height2">
+                    <img src="Material/0LngeNchtDrMseenStgt/MercedesMuseum.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item">
+                    <img src="Material/0WennManNachts/IMG_5264.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item">
+                    <img src="Material/2Amnesty/IMG_5259.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item grid-item--height2">
+                    <img src="Material/2Roadtrip/IMG_5247-2.jpg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item">
+                    <img src="Material/5Frau-Ada/FrauAda1.jpeg" style="width:100%; height:100%">
+                </div>
+                <div class="grid-item">
+                    <img src="Material/2Typo2/IMG_5287.jpg" style="width:100%; height:100%">
+                </div>
+
+            </div>
+        </div> 
+
+        <footer>
+            <p><b>Nice, that you made it to the end!</b></p>
+            <p><b>Now let's have a coffee.</b></p>
+        </footer>
+     </body>
+        
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE php>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name=“viewport” content=“width=device-width, initial-scale=1, user-scalable=yes”>
+        <title>Cara Kollmann</title>
+        <script src="packery.pkgd.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
+<body>
+     <div class="w3-content w3-opacity" style="max-width:1500px">
+        <header>
+            <div class="w3-bar">
+              <a href="index.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white"><h1><b>CARA KOLLMANN</b></h1></a>
+              <a href="Kontakt.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white w3-right"><h1><b>KONTAKT</b></h1></a>
+              <a href="About.html" class="w3-bar-item w3-button w3-hover-none w3-opacity w3-hover-text-white w3-right"><h1><b>ABOUT</b></h1></a>
+            </div>
+        </header> 
+     </div>
+  </body>
+</html>
