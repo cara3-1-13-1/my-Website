cfe651238f54aed0e218e8b0a42830a7a5343698
cara3-1-13-1 <65281201+cara3-1-13-1@users.noreply.github.com>
2020-06-15 11:29:07 +0200

    added class "grid-item-hover"

--- index.php
+++ index.php
@@ -18,62 +18,78 @@
         <!-- Packery -->
         <div class="grid" data-packery='{ "itemSelector": ".grid-item", "gutter": 10 }'>    
             <div class="grid">
-
-                <div class="grid-item grid-item--width2">
-                      <img src="Material/2Roadtrip/IMG_5251.jpg" style="width:100%; height:100%" class="image">
+                <div class="grid-item grid-item--width2 grid-item--height2 grid-item-hover">
+                    <img src="Material/5Coffea/Coffea-7209.jpg" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="Coffea.php"><h3 class="centered">COFFEA</h3></a>
+                    </div>
+                </div>
+                <div class="grid-item grid-item--width2 grid-item-hover">
+                    <img src="Material/4This-is-Europe/ThisIsEurope-7163.jpg" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="This-is-Europe.php"><h3 class="centered">THIS IS EUROPE</h3></a>
+                    </div>
+                </div>
+                <div class="grid-item grid-item--width2 grid-item--height2 grid-item-hover">
+                    <img src="Material/3Type-Inspiration/Secimen-angepasst.png" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="Cactaceae.php"><h3 class="centered">CACTACEAE</h3></a>
+                    </div>
+                </div>
+                <div class="grid-item grid-item--height2 grid-item-hover">
+                      <img src="Material/2Roadtrip/Roadtrip-gif.gif" style="width:100%; height:100%" class="image">
                       <div class="mouseover">
                           <a href="Roadtrip.php"><h3 class="centered">ROADTRIP</h3></a>
                     </div>
                 </div>
-                <div class="grid-item grid-item--height2">
-                    <img src="Material/0Milla/Plakat%20A0%202-remastered2020.jpg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">OHNE WORTE</h3></div>
-                </div>
-                <div class="grid-item">
-                    <img src="Material/1Sternenkarte/Sternenkarte-7133.jpg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">STERNENKARTE</h3></div>
-                </div>
-                <div class="grid-item grid-item--width2 grid-item--height2">
-                    <img src="Material/3Type-Inspiration/Secimen-angepasst.png" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">CACTACEAE</h3></div>
-                </div>
-                <div class="grid-item grid-item--width2 grid-item--height2">
-                    <img src="Material/5Coffea/Coffea-7209.jpg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">COFFEA</h3></div>
-                </div>
-                <div class="grid-item grid-item--width2">
-                    <img src="Material/4This-is-Europe/ThisIsEurope-7163.jpg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">THIS IS EUROPE</h3></div>
-                </div>
-                <div class="grid-item">
-                    <img src="Material/2Amnesty/Amnesty-freigestellt.png" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">RELIGIONSFREIHEIT</h3></div>
-                </div>
-                <div class="grid-item grid-item--width2 grid-item--height2">
-                    <img src="Material/2PaperJam/20180720_KD-143.jpg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">PAPER JAM</h3></div>
-                </div>
-                <div class="grid-item grid-item--height2">
-                    <img src="Material/0LngeNchtDrMseenStgt/LNDM-freigestellt.png" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">LNGE NCHT DR MSEEN STGT</h3></div>
-                </div>
-                <div class="grid-item">
+                <div class="grid-item grid-item-hover">
                     <img src="Material/0WennManNachts/IMG_5264.jpg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">WENN MAN NACHTS NIHCT SCHLAFEN KANN</h3></div>
+                    <div class="mouseover">
+                        <a href="Wenn-man-nachts.php"><h3 class="centered">WENN MAN NACHTS NICHT SCHLAFEN KANN</h3></a>
+                    </div>
                 </div>
-                <div class="grid-item grid-item--height2">
-                    <img src="Material/2Roadtrip/IMG_5247-2.jpg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">ROADTRIP</h3></div>
+                <div class="grid-item grid-item-hover">
+                    <img src="Material/2Amnesty/Amnesty-freigestellt.png" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="Religionsfreiheit.php"><h3 class="centered">RELIGIONSFREIHEIT</h3></a>
+                    </div>
                 </div>
-                <div class="grid-item">
+                <div class="grid-item grid-item--width2 grid-item--height2 grid-item-hover">
+                    <img src="Material/2PaperJam/20180720_KD-143.jpg" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="Paper-Jam.php"><h3 class="centered">PAPER JAM</h3></a>
+                    </div>
+                </div>
+                <div class="grid-item grid-item--height2 grid-item-hover">
+                    <img src="Material/0LngeNchtDrMseenStgt/LNDM-freigestellt.png" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="LNGE-NCHT.php"><h3 class="centered">LNGE NCHT DR MSEEN STGT</h3></a>
+                    </div>
+                </div>
+                <div class="grid-item grid-item-hover">
+                    <img src="Material/1Sternenkarte/Sternenkarte-7133.jpg" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="Sternenkarte.php"><h3 class="centered">STERNENKARTE</h3></a>
+                    </div>
+                </div>
+                <div class="grid-item grid-item-hover">
                     <img src="Material/5Frau-Ada/FrauAda1.jpeg" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">FRAU ADA DENKT UNERHÖRTES</h3></div>
+                    <div class="mouseover">
+                        <a href="Frau-Ada.php"><h3 class="centered">FRAU ADA DENKT UNERHÖRTES</h3></a>
+                    </div>
                 </div>
-                <div class="grid-item">
+                <div class="grid-item grid-item-hover">
                     <img src="Material/2Typo2/typo2-freigestellt.png" style="width:100%; height:100%" class="image">
-                    <div class="mouseover"><h3 class="centered">ESSAY PUNKT</h3></div>
+                    <div class="mouseover">
+                        <a href="Essay-Punkt.php"><h3 class="centered">ESSAY PUNKT</h3></a>
+                    </div>
                 </div>
-
+                <div class="grid-item grid-item--height2 grid-item-hover">
+                    <img src="Material/0Milla/Plakat.jpg" style="width:100%; height:100%" class="image">
+                    <div class="mouseover">
+                        <a href="Ohne-Worte.php"><h3 class="centered">OHNE WORTE</h3></a>
+                    </div>
+                </div>
             </div>
         </div> 
 
