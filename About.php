0a4102a23abf69076a6f887469ddf94f675e2bc0
cara3-1-13-1 <65281201+cara3-1-13-1@users.noreply.github.com>
2020-07-13 13:00:37 +0200

    added content to about-page

--- About.php
+++ About.php
@@ -13,91 +13,44 @@
     
 <body style="background-color: teal">
     <div class="content">    
-    <?php include('header.php'); ?>
+        <?php include('header.php'); ?>
+
+            <div style="width:100vw; height:100px"></div>
+
+            <!-- Packery -->
+            <div class="grid" data-packery='{ "itemSelector": ".grid-item", "gutter": 10 }'>    
+                <div class="grid">
+                    <div class="grid-item grid-item--width2 grid-item--height2">
+                        <img src="Material/About/Cara-schwarz.jpg" style="width:100%; height:100%">
+                    </div>
+                    <div class="grid-item grid-item--width2 grid-item--height2">
+                        <h4><b>BILDUNG</b> <br>
+                            2016 Abitur <br>
+                            am Evangelischen Mörike Gymnasium Stuttgart <br>
+                            <br>
+                            seit Herbst 2017 <br>
+                            Studium an der <a href="https://hfg-karlsruhe.de/" class="no-underscore w3-hover-text-white">staatlichen Hochschule für Gestaltung Karlsruhe</a> <br>
+                            <br>
+                            <b>PRAKTIKA</b> <br>
+                            3. - 13. August 2015 <br>
+                            Freiwilliges Kurzpraktikum bei <a href="https://www.milla.de/" class="no-underscore w3-hover-text-white">Milla & Partner GmbH</a>, Stuttgart<br>
+                            <br>
+                            2. September - 11. Oktober <br>
+                            Praktikum bei <a href="https://www.slanted.de/" class="no-underscore w3-hover-text-white">Slanted Publischers</a>, Karlsruhe <br>
+                            <br>
+                            <b>KLIENTEN</b> <br>
+                            seit 2019 <br>
+                            <a href="https://de-de.facebook.com/HorizonsColony/" class="no-underscore w3-hover-text-white">Horizons Colony</a><br>
+                            Metalband aus Biberach an der Riß
+                        </h4>
+                    </div>
+                </div>
+            </div> 
+
         <div style="width:100vw; height:100px"></div>
-        
-        <div style="height:30vh" class="w3-center">
-              <img src="Material/2Roadtrip/Titel.png" style="height:30vh">
-        </div>
         <div style="width:100vw; height:100px"></div>
 
-        <!-- Packery -->
-        <div class="grid" data-packery='{ "itemSelector": ".grid-item", "gutter": 10 }'>    
-            <div class="grid">
-
-                <div class="grid-item grid-item--width2">
-                      <img src="Material/2Roadtrip/IMG_5251.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item grid-item--height2">
-                    <img src="Material/0Milla/Plakat%20A0%202-remastered2020.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item">
-                    <img src="Material/1Sternenkarte/Sternenkarte-7133.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item grid-item--width2 grid-item--height2">
-                    <img src="Material/3Type-Inspiration/IMG_5073.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item grid-item--width2 grid-item--height2">
-                    <img src="Material/5Coffea/Coffea-7241.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item grid-item--width2">
-                    <img src="Material/4This-is-Europe/ThisIsEurope-7163.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item grid-item--width2 grid-item--height2">
-                    <img src="Material/2PaperJam/20180720_KD-143.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item grid-item--height2">
-                    <img src="Material/0LngeNchtDrMseenStgt/MercedesMuseum.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item">
-                    <img src="Material/0WennManNachts/IMG_5264.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item">
-                    <img src="Material/2Amnesty/IMG_5259.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item grid-item--height2">
-                    <img src="Material/2Roadtrip/IMG_5247-2.jpg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item">
-                    <img src="Material/5Frau-Ada/FrauAda1.jpeg" style="width:100%; height:100%">
-                </div>
-                <div class="grid-item">
-                    <img src="Material/2Typo2/IMG_5287.jpg" style="width:100%; height:100%">
-                </div>
-
-            </div>
-        </div> 
-
-<div style="width:100vw; height:100px"></div>
-<div><h5><b>Impressum</b></h5>
-
-<h5>Angaben gem&auml;&szlig; &sect; 5 TMG</h5>
-<h6><p>Cara Kollmann<br />
-Hinterm Dorf 6<br />
-76199 Karlsruhe</p></h6>
-
-<h5>Kontakt</h5>
-<h6><p>Telefon: +49 (0) 15735513081<br />
-E-Mail: mail@carakollmann.de</p></h6>
-
-<h5>Verantwortlich f&uuml;r den Inhalt nach &sect; 55 Abs. 2 RStV</h5>
-<h6><p>Cara Kollmann<br />
-Hinterm Dorf 6<br />
-76199 Karlsruhe</p></h6>
-
-<h5>Haftung f&uuml;r Inhalte</h5> 
-    <h6><p>Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p></h6>
-    <h6><p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p></h6> 
-<h5>Haftung f&uuml;r Links</h5> 
-    <h6><p>Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p></h6> 
-    <h6><p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p></h6> 
-<h5>Urheberrecht</h5> 
-    <h6><p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r den privaten, nicht kommerziellen Gebrauch gestattet.</p></h6> 
-    <h6><p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p></h6>
-
-<h6><p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p></h6></div>
-        
-    <?php include('footer.php'); ?>
+        <?php include('footer.php'); ?>
     </div>
 </body>
         
